0b76d7a331f08701a1381ec2bc3c08dc96527548
Devdatta Kulkarni <devdattakulkarni@gmail.com>
2023-04-14 18:43:59 +0000

    CICD deploy steps updated

--- CICD/containerized/deploy.sh
+++ CICD/containerized/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -x
 echo "This is deploy step"
 ls
-export KUBECONFIG=kubeconfig.json
+export KUBECONFIG=kubeconfig1.json
 gcloud auth activate-service-account --key-file kubeconfig.json
 gcloud config set project <insert-your-gcloud-project-name>
 gcloud container clusters get-credentials <insert-your-cluster-name>  --zone=us-central1-b
 helm delete wordpress -n default -v6
-helm install wordpress wp-chart --kubeconfig=kubeconfig.json -n default -v6
+helm install wordpress wp-chart --kubeconfig=kubeconfig1.json -n default -v6
